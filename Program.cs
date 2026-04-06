68a8ef43e239f4984c3c449c4a32fdc558f0c268
Guilherme Santos da Silva <guilhermenovo663@gmail.com>
2026-04-06 14:11:30 -0300

    Primeira versão da calculadora

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Runtime.InteropServices.Marshalling;
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine("Primeiro valor: ");
+            float v1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Segundo valor: ");
+            float v2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+
+            float resultado = v1 + v2;
+            Console.WriteLine("O Resultado da Soma é" + resultado);
+        }
+    }
+}
+
