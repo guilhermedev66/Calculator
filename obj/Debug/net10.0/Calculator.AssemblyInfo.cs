db185b1ec3e492e2aa791fb1698564873aca26f7
Guilherme Santos da Silva <guilhermenovo663@gmail.com>
2026-04-08 12:45:42 -0300

    Calculator Final..

--- obj/Debug/net10.0/Calculator.AssemblyInfo.cs
+++ obj/Debug/net10.0/Calculator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19c96b25d0d39ecd5247da42660ff6b864286593")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e784a545131e083ec1f127e11dc1602cb75faa8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
